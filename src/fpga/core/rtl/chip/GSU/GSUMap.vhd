3f97bfff7ae6058f852fa60007117dabcc5186d1
terminator2k2 <48845336+terminator2k2@users.noreply.github.com>
2023-08-27 21:02:42 +0100

    Update GSUMap.vhd

--- src/fpga/core/rtl/chip/GSU/GSUMap.vhd
+++ src/fpga/core/rtl/chip/GSU/GSUMap.vhd
@@ -8,34 +8,34 @@ use IEEE.STD_LOGIC_TEXTIO.all;
 
 entity GSUMap is
 	port(
-		MCLK			: in std_logic;
-		RST_N			: in std_logic;
+		MCLK		: in std_logic;
+		RST_N		: in std_logic;
 		ENABLE		: in std_logic := '1';
 		
-		CA   			: in std_logic_vector(23 downto 0);
-		DI				: in std_logic_vector(7 downto 0);
-		DO				: out std_logic_vector(7 downto 0);
+		CA   		: in std_logic_vector(23 downto 0);
+		DI		: in std_logic_vector(7 downto 0);
+		DO		: out std_logic_vector(7 downto 0);
 		CPURD_N		: in std_logic;
 		CPUWR_N		: in std_logic;
 		
-		PA				: in std_logic_vector(7 downto 0);
+		PA		: in std_logic_vector(7 downto 0);
 		PARD_N		: in std_logic;
 		PAWR_N		: in std_logic;
 		
-		ROMSEL_N		: in std_logic;
-		RAMSEL_N		: in std_logic;
+		ROMSEL_N	: in std_logic;
+		RAMSEL_N	: in std_logic;
 		
 		SYSCLKF_CE	: in std_logic;
 		SYSCLKR_CE	: in std_logic;
 		REFRESH		: in std_logic;
 		
-		IRQ_N			: out std_logic;
+		IRQ_N		: out std_logic;
 
-		ROM_ADDR		: out std_logic_vector(22 downto 0);
-		ROM_Q			: in  std_logic_vector(15 downto 0);
-		ROM_CE_N		: out std_logic;
-		ROM_OE_N		: out std_logic;
-		ROM_WORD		: out std_logic;
+		ROM_ADDR	: out std_logic_vector(22 downto 0);
+		ROM_Q		: in  std_logic_vector(15 downto 0);
+		ROM_CE_N	: out std_logic;
+		ROM_OE_N	: out std_logic;
+		ROM_WORD	: out std_logic;
 		
 		BSRAM_ADDR	: out std_logic_vector(19 downto 0);
 		BSRAM_D		: out std_logic_vector(7 downto 0);
@@ -44,12 +44,12 @@ entity GSUMap is
 		BSRAM_OE_N	: out std_logic;
 		BSRAM_WE_N	: out std_logic;
 
-		MAP_ACTIVE  : out std_logic;
-		MAP_CTRL		: in std_logic_vector(7 downto 0);
-		ROM_MASK		: in std_logic_vector(23 downto 0);
+		MAP_ACTIVE      : out std_logic;
+		MAP_CTRL	: in std_logic_vector(7 downto 0);
+		ROM_MASK	: in std_logic_vector(23 downto 0);
 		BSRAM_MASK	: in std_logic_vector(23 downto 0);
 
-		TURBO		   : in std_logic;
+		TURBO		: in std_logic;
 		FASTROM   	: in std_logic
 	);
 end GSUMap;
@@ -68,32 +68,32 @@ begin
 	
 	GSU : entity work.GSU
 	port map(
-		CLK			=> MCLK,
-		RST_N			=> RST_N and MAP_SEL,
+		CLK		=> MCLK,
+		RST_N		=> RST_N and MAP_SEL,
 		ENABLE		=> ENABLE,
 
-		ADDR			=> CA,
-		DO				=> DO,
-		DI				=> DI,
-		RD_N			=> CPURD_N,
-		WR_N			=> CPUWR_N,
+		ADDR		=> CA,
+		DO		=> DO,
+		DI		=> DI,
+		RD_N		=> CPURD_N,
+		WR_N		=> CPUWR_N,
 		
 		SYSCLKF_CE	=> SYSCLKF_CE,
 		SYSCLKR_CE	=> SYSCLKR_CE,
 		
-		IRQ_N			=> IRQ_N,
+		IRQ_N		=> IRQ_N,
 		
-		ROM_A			=> ROM_A,
+		ROM_A		=> ROM_A,
 		ROM_DI		=> ROM_Q(7 downto 0),
-		ROM_RD_N		=> ROM_OE_N,
+		ROM_RD_N	=> ROM_OE_N,
 		
-		RAM_A			=> RAM_A,
+		RAM_A		=> RAM_A,
 		RAM_DI		=> BSRAM_Q,
 		RAM_DO		=> BSRAM_D,
-		RAM_WE_N		=> RAM_WE_N,
-		RAM_CE_N		=> BSRAM_CE_N,
+		RAM_WE_N	=> RAM_WE_N,
+		RAM_CE_N	=> BSRAM_CE_N,
 				
-		TURBO			=> TURBO,
+		TURBO		=> TURBO,
 		FASTROM		=> FASTROM
 	);
 	
